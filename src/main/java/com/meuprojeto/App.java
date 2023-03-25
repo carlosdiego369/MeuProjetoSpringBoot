5f7319b6fdf014a71b70b8016ac31ac53f4074a9
carlosdiego <carlos.diego@ibrowse-sds.com.br>
2023-03-25 15:33:54 -0300

    Entity Finalizada

--- src/main/java/com/meuprojeto/App.java
+++ src/main/java/com/meuprojeto/App.java
@@ -1,11 +1,23 @@
 package com.meuprojeto;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class App {
+    @Value("${paginacao.qtd_por_pagina}")
+    private int qtdPorPagina;
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
     }
+
+//    @Bean
+//    public CommandLineRunner commandLineRunner() {
+//        return args -> {
+//            System.out.println("### Quantidade de elementos por página = " + this.qtdPorPagina);
+//        };
+//    }
 }
